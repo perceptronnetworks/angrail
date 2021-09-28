f8ea46d99570051f013ecaa92b4d73bb4dac6fe6
spark_hema <hemalatha@sparktech.com.hk>
2021-09-29 00:17:04 +0530

    Template created for sub menus

---
+++ wp-content/themes/angrail/page-augumented-reality.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: Augmented Reality and Virtual Reality 
+ */
+get_header();
+?>
+<main id="primary" class="site-main">
+<body id="top">
+<div class="wrapper bgded overlay" style="background-image:url('../images/demo/backgrounds/01.png');">
+  <div id="breadcrumb" class="hoc clear"> 
+      <h2><?php echo get_the_title(); ?></h2>
+  </div>
+</div>
+<div class="wrapper row3">
+  <div class="hoc page-container clear"> 
+    <div class="content about_align"> 
+      <div class="two_quarter">
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+    <div class="two_quarter">
+        <img src="/wp-content/uploads/2021/09/dylan-gillis-KdeqA3aTnBY-unsplash-scaled.jpg">
+        </div>
+    </div>
+    <div class="clear"></div>
+</div>
+</main >
+</div>
+</body>
+</main><!-- #main -->
+<?php
+get_sidebar();
+
+get_footer();
---
+++ wp-content/themes/angrail/page-patent-and-ipr.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Patent & IPR
+ */
+get_header();
+?>
+<main id="primary" class="site-main">
+<body id="top">
+<div class="wrapper bgded overlay" style="background-image:url('../images/demo/backgrounds/01.png');">
+  <div id="breadcrumb" class="hoc clear"> 
+      <h2><?php echo get_the_title(); ?></h2>
+  </div>
+</div>
+</main >
+</div>
+</body>
+</main><!-- #main -->
+<?php
+get_sidebar();
+
+get_footer();
