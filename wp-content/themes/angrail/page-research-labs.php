ca00a0c625f476f0cce7dfe343afaea9b50c6dd7
spark_hema <hemalatha@sparktech.com.hk>
2021-09-28 23:34:08 +0530

    Page Style updated

--- wp-content/themes/angrail/page-research-labs.php
+++ wp-content/themes/angrail/page-research-labs.php
@@ -13,7 +13,11 @@ get_header();
 </div>
 <div class="wrapper row3">
   <div class="hoc page-container clear"> 
-    <div class="content research_align"> 
+    <div class="content about_align"> 
+    <div class="two_quarter">
+            <img src="/wp-content/uploads/2021/09/cdc-rHCNMH9Vlmw-unsplash-scaled.jpg">
+    </div>
+    <div class="two_quarter">
     <p>ANGRAIL aims is to create a global standard research lab for Universities, Colleges and Institutions across the world. Our Research Lab facilities are spread across trending areas such as Robotics, IoT, AI & ML, Cyber security, AR & VR, Hardware & Networking, Cloud Computing, Renewable Energy, etc. We have an experienced team belongs to Engineering, Science, Arts, Education, Management, Agriculture, Animal Husbandry, Medical and Allied Health Sciences.</p>
 
     <p>ANGRAIL offer labs in three modes, namely, Virtual Lab, Remote Lab and Hybrid Labs. Virtual Labs will have the essential trending software tools. A remote lab is a remotely operated physical lab, where the physical equipment shall be accessed anywhere in the world over the internet at any time. Our Hybrid labs is a combination of Virtual and Remote labs. Students will learn the essential software and access the physical equipment to learn the skills. Any school, college and PhD student shall access the lab any time.</p>
@@ -21,19 +25,7 @@ get_header();
     <p>The school Labs are designed for improving Science, Technology, Engineering, Arts and Mathemmatics (STEAM) Education and Creativity skills among students. Our carefully designed special project activities will complement the lab activities. Our labs motivate multi-disciplinary participation to enhance the motivation among students. The whole lab shall be accessed online at the ease of the home, and offline mode is also available. </p>
 
     <p>Our Lab facilities offer comprehensive research facilities to the UG, PG and PhD students. Students will have bridge lab sessions using our school labs before entering into college-level labs. Our college-level virtual labs will have essential software tools like Python, Java, C, Scilab, Blender, R, and so on. The Remote Labs have world-class equipment from IoT, Robotics, Cyber Security and so on. </p>
-    <h4 style="margin:25px 0px 10px 0px;">Research Lab Techniques</h4>
-    <ol style="padding:20px;">
-    <li>Design, Development and Application of IoT</li>
-    <li>Modelling, Analysis, Design and Applications of Robots</li>
-    <li>Autonomous Robots: AI & ML</li>
-    <li>Design and Application of Renewable Energy Systems</li>
-    <li>Augmented Reality and Virtual Reality </li>
-    <li>Advanced Programming on Network, Security, Digital Forensic and Ethical Hacking</li>
-    <li>Webinars </li>
-    <li>Workshops</li>
-    <li>FDP</li>
-    </ol>
-
+</div>
     </div>
     <div class="clear"></div>
 </div>
--- wp-content/themes/angrail/page-services.php
+++ wp-content/themes/angrail/page-services.php
@@ -14,8 +14,13 @@ get_header();
 
 <div class="wrapper row3">
   <div class="hoc page-container clear"> 
-    <div class="content"> 
-    <p>ANGRAIL offers the state of the art services to schools, colleges, industries and individuals in the field of Research, Creativity and Innovation. Our services expand from technical training for the faculty members, focused special training on the recent trending topics, internships to the college students, sales and services of the education instruments. Our services come with a warranty and after-sales service support. We have international tie-ups with the top-class product suppliers in the world. We supply our products globally.</p>
+    <div class="content about_align"> 
+    <div class="two_quarter">
+        <img src="/wp-content/uploads/2021/09/luke-van-zyl-5916rBUU3CM-unsplash-scaled.jpg">
+        </div>
+      <div class="two_quarter">
+        <p>ANGRAIL offers the state of the art services to schools, colleges, industries and individuals in the field of Research, Creativity and Innovation. Our services expand from technical training for the faculty members, focused special training on the recent trending topics, internships to the college students, sales and services of the education instruments. Our services come with a warranty and after-sales service support. We have international tie-ups with the top-class product suppliers in the world. We supply our products globally.</p>
+        </div>
     </div>
     <div class="clear"></div>
 </div>
