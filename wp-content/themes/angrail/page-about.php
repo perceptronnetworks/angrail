ab9c29df9c2beeadfbfd89af9557e6ce62017969
spark_hema <hemalatha@sparktech.com.hk>
2021-09-29 23:22:41 +0530

    pages updated

--- wp-content/themes/angrail/page-about.php
+++ wp-content/themes/angrail/page-about.php
@@ -32,7 +32,6 @@ get_header();
   <div class="hoc page-container clear"> 
     <div class="content about_align"> 
     <div class="two_quarter">
-            <h2>Research Door</h2>
             <p>To create the key to open the research door, Our CEO, Dr R. Kavitha, envisioned a global platform for imparting young minds' with Research, Creativity, and Innovation. This initiative aims to "Kindle Innovation & Creativity among students to develop Next Generation International Researchers and Innovators." Our CEO has 20+ years of experience in academics and Research. She worked in Indian and African countries as a Teacher, Professor and Principal. Now, she is working as a Vice-Chancellor of DMI-St. Eugene University, Zambia.  From the initial stages of her career, she expresses kindness and cares towards students. At the same time, she ensures the students attain the highest possible level of learning.</p>
             <p>ANGRAIL follows a carefully designed pedagogy. We identified emerging skills such as IoT, Robotics, AI & ML, Renewable Energy, AR, VR, Cloud Computing and Cyber Security as the much-needed skills of this millennium. We create a global platform for any individual to access the state of the art lab facilities with gamified learning at the ease to their home. Our state of the art initiatives such as Research Laboratory, School and College Projects, Online-Offline-Blended Courses, Research Services, hardware and Software Services empowers the students to compete in the international space. Our outcome-based teaching-learning method will equip the students to meet the international standards and provide confidence among them. Our initiative will prepare the next generation talent pool with global standards, making the country a global hub for Research, Creativity and Innovation in all domains.</p>
         </div>
@@ -41,41 +40,72 @@ get_header();
             <img src="/wp-content/uploads/2021/09/kavitha.jpeg">
         </div>
         
-      
     </div>
     <div class="clear"></div>
+
 </div>
 </div>
 <div class="wrapper row3">
   <main  class="hoc page-container clear"> 
     <div class="content about_align" > 
+    <div class="two_quarter">
+            <img src="wp-content/uploads/2021/09/zsun-fu-1fg5GuXI2BU-unsplash-scaled.jpg">
+        </div>
         <div class="two_quarter">
-        <h2>Mission</h2>
-        <p>
-        <ul>
-            <li>To establish state of art research facilities for easy access to national and international researchers;</li>
-            <li>To impart unique learning experience to bring in novel ideas and create a platform to students who wish to undertake innovation;</li>
-            <li>To promote advanced research, training and mentorship for innovation and skill enhancement;</li>
-            <li>To enrich knowledge and technical skills to take up their own Projects and Products;</li>
-            <li>To collaborate with the industries and institutes on developing solutions for problems;</li>
-            <li>To cultivate entrepreneurial skill and encourage students towards business start-ups.</li>
-        </ul>
-        </p>
+          <h2>Mission</h2>
+            <p>
+            <ul>
+                <li>To establish state of art research facilities for easy access to national and international researchers;</li>
+                <li>To impart unique learning experience to bring in novel ideas and create a platform to students who wish to undertake innovation;</li>
+                <li>To promote advanced research, training and mentorship for innovation and skill enhancement;</li>
+                <li>To enrich knowledge and technical skills to take up their own Projects and Products;</li>
+                <li>To collaborate with the industries and institutes on developing solutions for problems;</li>
+                <li>To cultivate entrepreneurial skill and encourage students towards business start-ups.</li>
+            </ul>
+          </p>
         </div> 
-        <div class="two_quarter">
-        <h2>Objectives</h2>
-        <ul>
-        <li>Provide state of art laboratory infrastructure to encourage students to experiment the fundamental application of science, technology, engineering and mathematics, and enable product visualization;</li>
-        <li>Empower students with knowledge of fundamentals to latest technologies in their respective areas of interest and enable them to be solution providers, engineers, scientist and entrepreneur.</li>
-        <li>Connect schools and colleges and provide opportunity to work on their ideas as projects and Strengthen the innovation ecosystem;</li>
-        <li>Educate & Train next generation innovators with latest research facilities under one roof in transfering idea into product and commercialization;</li>
-        <li>Promote Research and enhance competence and technical skills in recent technology and progress in their respective fields of interest.</li>
-        <li>Develop innovative products and provide expert solutions to fulfill the emerging customer needs and industries with the latest technology in the relevant fields.</li>
-        <li>Provide opportunities to students with various avenues to participate in national and international technical contests;</li>
-        <li>Establish and implement strategies to translate knowledge & research skills into new products and generate Patents & IPRs.</li>
-        </p>
-        </div>   
-    </div> 
+</div> 
+    <div class="clear"></div>
+</main>
+</div>
+
+<div class="wrapper row3">
+  <main  class="hoc page-container clear"> 
+    <div class="content about_align" > 
+    <div class="two_quarter">
+            <h2>Objectives</h2>
+            <ul>
+            <li>Provide state of art laboratory infrastructure to encourage students to experiment the fundamental application of science, technology, engineering and mathematics, and enable product visualization;</li>
+            <li>Empower students with knowledge of fundamentals to latest technologies in their respective areas of interest and enable them to be solution providers, engineers, scientist and entrepreneur.</li>
+            <li>Connect schools and colleges and provide opportunity to work on their ideas as projects and Strengthen the innovation ecosystem;</li>
+            <li>Educate & Train next generation innovators with latest research facilities under one roof in transfering idea into product and commercialization;</li>
+            <li>Promote Research and enhance competence and technical skills in recent technology and progress in their respective fields of interest.</li>
+            <li>Develop innovative products and provide expert solutions to fulfill the emerging customer needs and industries with the latest technology in the relevant fields.</li>
+            <li>Provide opportunities to students with various avenues to participate in national and international technical contests;</li>
+            <li>Establish and implement strategies to translate knowledge & research skills into new products and generate Patents & IPRs.</li>
+            </ul>
+        </div> 
+    <div class="two_quarter">
+            <img src="wp-content/uploads/2021/09/scott-graham-5fNmWej4tAA-unsplash-scaled.jpg">
+        </div>
+</div> 
+    <div class="clear"></div>
+    </main>
+</div>
+
+
+    <div class="wrapper row3">
+     <main  class="hoc page-container clear"> 
+    <div class="content about_align" > 
+    <div class="two_quarter">
+            <img src="wp-content/uploads/2021/09/nick-morrison-FHnnjk1Yj7Y-unsplash-1-scaled.jpg">
+        </div>
+    <div class="two_quarter">
+            <h2>Vision</h2>
+          <p>Kindle Innovation & Creativity among students to develop the Next Generation International Researchers and Innovators.</p>
+        </div> 
+
+</div> 
     <div class="clear"></div>
 </main >
 </div>
