8597ce9d9ced40a5dca6ffacb6ea453688737bc9
spark_hema <hemalatha@sparktech.com.hk>
2021-09-29 22:42:34 +0530

    Home updated

--- wp-content/themes/angrail/page-home.php
+++ wp-content/themes/angrail/page-home.php
@@ -26,7 +26,7 @@ get_header();
   </div>
 </div>
 
-<div class="wrapper row2">
+<!-- <div class="wrapper row2">
   <section id="introblocks" class="hoc container clear"> 
    
     <ul class="nospace group">
@@ -53,7 +53,7 @@ get_header();
       </li>
     </ul>
   </section>
-</div>
+</div> -->
 
 <div class="wrapper row3">
   <main class="hoc container clear"> 
