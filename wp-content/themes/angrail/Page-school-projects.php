c1d8bf83e0e266ece8775967572bc3524823c054
spark_hema <hemalatha@sparktech.com.hk>
2021-09-30 17:59:51 +0530

    School projects updated

--- wp-content/themes/angrail/Page-school-projects.php
+++ wp-content/themes/angrail/Page-school-projects.php
@@ -27,13 +27,14 @@ get_header();
 <div class="wrapper row3">
   <div class="hoc page-container clear"> 
     <div class="content about_align"> 
-    <div class="two_quarter">
-            <img src="/wp-content/uploads/2021/09/ousa-chea-gKUC4TMhOiY-unsplash-scaled.jpg">
-    </div>
+    
     <div class="two_quarter">
     <h6>Science Project</h6>
     <p>We deal with all types of arts, science, engineering and technology projects involving electronic, electrical, renewable energy, physics, chemistry, biology, computer science, plastics, robotics, glass, clay, painting, etc. Every student will get one individual mentor throughout the project work. We teach the skills needed, supply all required hardware and manual for operating the hardware also with students. Upon completion of the projects, students can publish the works in blogs and social media pages.  Our science projects enable any student in the world to connect with any other students.  Students from 3rd grade shall participate in this project. </p>
     </div> 
+    <div class="two_quarter">
+            <img src="/wp-content/uploads/2021/09/ousa-chea-gKUC4TMhOiY-unsplash-scaled.jpg">
+    </div>
     </div>
     <div class="clear"></div>
 </div>
@@ -41,26 +42,12 @@ get_header();
 <div class="wrapper row3">
   <div class="hoc page-container clear"> 
     <div class="content about_align"> 
-    <div class="two_quarter">
-    <h6>Mathematics Projects</h6>
-    <p>Our mathematics projects involve both hardware and software elements. We carefully designed the projects for enriching the mathematical application-oriented learning among student communities. Our pool of experts across the globe will offer the state of the art project experience to students. Our project topics involve exciting applications, such as the application of matrix, differentiation, integration, vectors, complex numbers, numerical methods, statistics, probability, etc. We have mathematics projects from elementary to higher secondary students. </p>
-    </div> 
     <div class="two_quarter">
             <img src="/wp-content/uploads/2021/09/jeswin-thomas-hecib2an4T4-unsplash-scaled.jpg">
     </div>
-    </div>
-    <div class="clear"></div>
-</div>
-
-<div class="wrapper row3">
-  <div class="hoc page-container clear"> 
-    <div class="content about_align"> 
     <div class="two_quarter">
-            <img src="/wp-content/uploads/2021/09/yulian-karadzhov-RNBZ63yCKcc-unsplash-scaled.jpg">
-    </div>
-    <div class="two_quarter">
-    <h6>Thermocol Projects</h6>
-    <p>Thermocol projects will be used for building the structures, elements and other prototypes of any mathematics, arts, craft and science project. Most of the time thermocol projects shall be a compliment for arts, science and mathematics. We have thermocol projects from elementary to higher secondary students. </p>
+    <h6>Mathematics Projects</h6>
+    <p>Our mathematics projects involve both hardware and software elements. We carefully designed the projects for enriching the mathematical application-oriented learning among student communities. Our pool of experts across the globe will offer the state of the art project experience to students. Our project topics involve exciting applications, such as the application of matrix, differentiation, integration, vectors, complex numbers, numerical methods, statistics, probability, etc. We have mathematics projects from elementary to higher secondary students. </p>
     </div> 
     
     </div>
@@ -70,13 +57,29 @@ get_header();
 <div class="wrapper row3">
   <div class="hoc page-container clear"> 
     <div class="content about_align"> 
+    
+    <div class="two_quarter">
+    <h6>Thermocol Projects</h6>
+    <p>Thermocol projects will be used for building the structures, elements and other prototypes of any mathematics, arts, craft and science project. Most of the time thermocol projects shall be a compliment for arts, science and mathematics. We have thermocol projects from elementary to higher secondary students. </p>
+    </div> 
+    <div class="two_quarter">
+            <img src="/wp-content/uploads/2021/09/yulian-karadzhov-RNBZ63yCKcc-unsplash-scaled.jpg">
+    </div>
+    </div>
+    <div class="clear"></div>
+</div>
+
+<div class="wrapper row3">
+  <div class="hoc page-container clear"> 
+    <div class="content about_align"> 
+    <div class="two_quarter">
+            <img src="/wp-content/uploads/2021/09/Charles-Young-Tiny-Paper-Models-Paperholm-4.jpg">
+    </div>
     <div class="two_quarter">
     <h6>Cut Out Models</h6>
     <p>Cut out models is the activity that will enable the school kids to apply their creativity. As we know, the cognitive skills of a kid will be entirely different for kids, and the cutout model is the starting point for manifesting the kid's talent. Out creative and straightforward to do cut out models will be exciting and fun. </p>
     </div> 
-    <div class="two_quarter">
-            <img src="/wp-content/uploads/2021/09/Charles-Young-Tiny-Paper-Models-Paperholm-4.jpg">
-    </div>
+    
     </div>
     <div class="clear"></div>
 </div>
